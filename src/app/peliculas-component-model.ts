45f640a252bfd73129e1800ab2fc140dee8430c9
VALLEJO GUTIERREZ Monica <monica.vallejo@soprasteria.com>
2017-09-12 15:24:35 +0200

    changes

--- src/app/peliculas-component-model.ts
+++ src/app/peliculas-component-model.ts
@@ -1,13 +1,16 @@
 export class PeliculasComponentModel{
 
-    titulo : string;
-    director : string;
-    categoria : Categoria;
+   // titulo : string;
+   // director : string;
+   // categoria : Categoria;
 
-    constructor(titulo:string, director:string,  categoria:Categoria){
-        this.titulo = titulo;
-        this.director = director;        
-        this.categoria = categoria;
+// constructor( titulo:string, director:string, categoria:Categoria){
+    //   this.titulo = titulo;
+    //  this.director = director;        
+    //  this.categoria = categoria;
+//  }
+
+    constructor(public titulo:string, public director:string, public categoria:Categoria){
     }
 }
 
--- src/app/prueba/prueba.component.ts
+++ src/app/prueba/prueba.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { PeliculasComponentModel } from "../peliculas-component-model";
 import { ServicioPruebaService } from "../servers/servicio-prueba.service";
 
@@ -10,23 +10,24 @@ import { ServicioPruebaService } from "../servers/servicio-prueba.service";
 })
 
 
-export class PruebaComponent implements OnInit {
+export class PruebaComponent implements OnInit, OnChanges {
 
   //importamos variable del appcomponent
   @Input() titulo : string;
   @Input() variable: string;
  
- 
-
-
   //Se recomineda declarar las variables a undefinded, no a null
   variable_null: number = null; 
   variable_undefined : number = undefined;
-  titulo_ ;
+  tituloOnInit;
+  tituloConstructor;
+
+  contador_constructor:number ;
+  contador_oninit:number;
 
   peliculas:PeliculasComponentModel[];
-
   peliculasServicio: Array<PeliculasComponentModel>;
+  peliculasServicioConstructor : PeliculasComponentModel[];
 
   //CONSTRUCTOR
   //inyeccion de dependencias
@@ -39,17 +40,61 @@ export class PruebaComponent implements OnInit {
       new PeliculasComponentModel("Pelicula2","director2","Terror"),
       new PeliculasComponentModel("Pelicula3","director3","Historica")
     ];
+    this.tituloConstructor = "titulo en el constructor -> " + this.titulo;
+    this.peliculasServicioConstructor = this.servicioPrueba.crearPeliculas();
    }
+
    
-   
+   ngOnChanges(changes : SimpleChanges){
+      console.log("ngOnchanges");
+      console.log(changes);
+   }
+
 
   //NGONINIT
   //realizamos inicializaciones complejas-> tipo obtener datos por medio de un servicio
   //obtener valores de las propiedades del tipo Input
   ngOnInit(){
+    console.log("ngOnInit");
     this.peliculasServicio = this.servicioPrueba.crearPeliculas();
-    this.titulo_ = "este es el titulo -> " + this.titulo;
+    this.tituloOnInit = "titulo en el ngOnInit -> " + this.titulo;
+    //console.log(typeof this.tituloOnInit);
+    //var result = this.errorsOnThree(3);
+    //var result2 = this.errorsOnThreeClass(3);
+
+    
+    // try {
+    //   var result = this.errorsOnThreeClass(3);
+    // }catch (err) {
+    //   if ((err instanceof ApplicationError)) {
+    //   throw err;
+    // }
+    //   console.log('Error caught, no action taken');
+    // }
   }
 
+   errorsOnThree(input: number) {
+      if (input === 3) {
+        throw new Error('Three is not allowed');
+      }
+      return input;
+    }
+
+    errorsOnThreeClass(input: number) {
+      if (input === 3) {
+        throw new ApplicationError('Three is not allowed');
+      }
+      return input;
+    }
 }
  
+class ApplicationError implements Error {
+  public name = 'ApplicationError';
+
+  constructor(public message: string) {
+  }
+
+  toString() {
+    return this.name + ': ' + this.message;
+  }
+}
